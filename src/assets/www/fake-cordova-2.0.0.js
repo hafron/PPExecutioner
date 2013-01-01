39554d6cf08be741006d69344ddd74b742070d71
Szymon Olewniczak <szymon.olewniczak@rid.pl>
2013-01-01 15:20:07 +0100

    it works

---
+++ src/assets/www/fake-cordova-2.0.0.js
@@ -0,0 +1,15 @@
+Connection = new Object();
+Connection.NONE = 1
+navigator = new Object();
+navigator.network = new Object();
+navigator.network.connection = new Object();
+navigator.network.connection.type = 2
+
+Media = function(path)
+{
+
+    this.play = function()
+    {}
+
+}
+
